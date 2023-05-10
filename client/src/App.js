3337c984760d2fc87ceba4d2243b574d89602eb8
Nickon Babaei <babaein1@gmail.com>
2023-05-10 13:20:01 -0300

    added skeleton to all components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Nav />
+      
+    </div>
   );
 }
 
