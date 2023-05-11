9b5a082c4b5207d2c3ed893e7a095186ee933f7f
Nickon Babaei <babaein1@gmail.com>
2023-05-11 13:56:47 -0300

    made about section responsive

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,21 +4,19 @@ import headshot from '../images/headshot-removebg.png'
 const About = () => {
     return (
         <section class="text-gray-600 body-font bg-gradient-to-b from-emerald-200 via-emerald-100 to-emerald-50">
-            <div class="container mx-auto flex px-5 items-center justify-center flex-col">
-                <div class='mb-8'>
+            <div class='mb-8'>
                     <h1>About Me</h1>
                 </div>
-               
-                    <img class="lg:w-3/6 md:w-4/6 w-5/6 mb-10 object-cover object-center rounded" alt="headshot" src={headshot} />
+            <div class="md:container md:flex md:mx-auto md:px-5 md:items-center md:justify-center md:flex">
+                
+                
+                    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="headshot" src={headshot} />
                 
 
                 <div class="text-center lg:w-2/3 w-full">
                     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
                     <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
-                    <div class="flex justify-center">
-                        <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
-                        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
-                    </div>
+                   
                 </div>
             </div>
         </section>
