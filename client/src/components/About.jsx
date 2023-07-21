cfee3981834f891fd53b19d8830f9709952849d0
Nickon Babaei <babaein1@gmail.com>
2023-07-21 00:01:07 -0300

    changed about me info

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -13,9 +13,14 @@ const About = () => {
                     <img class="mx-auto mb-4 lg:mb-0 object-cover object-center rounded" alt="headshot" src={headshot} />
                 </div>
 
-                <div class="text-center lg:w-2/5 lg:mr-16 w-full">
-                    <p class="leading-relaxed pb-4 sm:text-lg text-base">After recently obtaining a degree in mathematics, I ran into an old friend who introduced me to my newfound passion for coding. The logical thinking and problem-solving skills honed during my studies seamlessly transitioned into the world of programming. The synergy between mathematics and coding deeply fascinate me. To be able to turn essentially any idea into a fixed reality, really resonates with me.</p>
-                    <p class="leading-relaxed sm:text-lg text-base">After recently obtaining a degree in mathematics, I ran into an old friend who introduced me to my newfound passion for coding. The logical thinking and problem-solving skills honed during my studies seamlessly transitioned into the world of programming. The synergy between mathematics and coding deeply fascinate me. To be able to turn essentially any idea into a fixed reality, really resonates with me.</p>
+                <div class="lg:w-2/5 lg:mr-16 w-full">
+                    
+                    <p class="leading-relaxed sm:text-lg text-base text-black font-semibold pb-4">I am a Software Engineer who recently completed General Assembly's Software 
+                    Engineering Immersive Bootcamp and a Math degree from Queen's University. 
+                    </p> 
+                    <p class="leading-relaxed sm:text-lg text-base pb-4">After recently obtaining a degree in mathematics, I ran into an old friend who introduced me to my newfound passion for coding. The logical thinking and problem-solving skills honed during my studies seamlessly transitioned into the world of programming. The synergy between mathematics and coding deeply fascinate me. To be able to turn essentially any idea into a fixed reality, really resonates with me.</p>
+                    <p class="leading-relaxed sm:text-lg text-base">In my spare time, I find joy in various activities such as playing basketball, cooking, snowboarding, hiking, and traveling to new and exciting destinations. My fascination with technology extends beyond my professional life, as I constantly seek out opportunities to engage with the latest and coolest tech innovations.
+                    </p> 
                 </div>
             </div>
         </section>
