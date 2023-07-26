c170392b0d0a172db2334cd6e880f542b6ac3639
Nickon Babaei <babaein1@gmail.com>
2023-07-25 23:29:42 -0300

    got rid unnecessary code in Hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -6,7 +6,6 @@ const Hero = () => {
         <section className="bg-gradient-to-b from-emerald-600 via-emerald-400 to-emerald-300 text-white" id='home'>
             <div
                 className="flex mx-auto max-w-full px-4 py-32 h-screen items-start"
-                // style={{ backgroundImage: `url(${heroGif})`, backgroundSize: 'cover' }}
             >
 
                 <div className="mx-auto flex flex-col max-w-3xl text-center backdrop-blur-md pb-8 pt-4 px-4 md:grid">
@@ -15,14 +14,6 @@ const Hero = () => {
                         <img src={waveGif} className='w-16 sm:w-24'/>
                         </div>
                         <h1 className='text-white font-extrabold text-5xl sm:text-7xl sm:pl-6'>I'm Nickon</h1>
-                        
-                         
-
-                        {/* <span className="sm:block"> Increase Conversion. </span> */}
-                    
-
-                    
-                    
 
                     <p className="mx-auto mt-4 max-w-md font-bold sm:text-xl/relaxed">
                         Software Engineer 
@@ -65,8 +56,6 @@ const Hero = () => {
                         <a href="https://www.linkedin.com/in/nickon-babaei/" target="_blank" rel="noreferrer" >
                             <div class="flex gap-3 items-center h-10 hover:scale-110">
                             <img src={resume} className='h-20'/>
-                                {/* <svg width="24" height="24" fill="white" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" ><path d="M3 24h19v-23h-1v22h-18v1zm17-24h-18v22h18v-22zm-1 1h-16v20h16v-20zm-2 16h-12v1h12v-1zm0-3h-12v1h12v-1zm0-3h-12v1h12v-1zm-7.348-3.863l.948.3c-.145.529-.387.922-.725 1.178-.338.257-.767.385-1.287.385-.643 0-1.171-.22-1.585-.659-.414-.439-.621-1.04-.621-1.802 0-.806.208-1.432.624-1.878.416-.446.963-.669 1.642-.669.592 0 1.073.175 1.443.525.221.207.386.505.496.892l-.968.231c-.057-.251-.177-.449-.358-.594-.182-.146-.403-.218-.663-.218-.359 0-.65.129-.874.386-.223.258-.335.675-.335 1.252 0 .613.11 1.049.331 1.308.22.26.506.39.858.39.26 0 .484-.082.671-.248.187-.165.322-.425.403-.779zm3.023 1.78l-1.731-4.842h1.06l1.226 3.584 1.186-3.584h1.037l-1.734 4.842h-1.044z" /></svg> */}
-
                             </div>
                         </a>
 
@@ -75,7 +64,6 @@ const Hero = () => {
                 </div>
 
                 
-
             </div>
             <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2">
                     <svg className="w-12 h-12 text-white animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
