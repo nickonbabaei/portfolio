efc35fb8b8ed3c13231ca07a9caba6cfb1ecd886
Nickon Babaei <babaein1@gmail.com>
2023-08-09 11:32:41 -0300

    deleted pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import {Route, Routers} from 'react-router-dom'
 
 function App() {
   return (
@@ -15,6 +15,9 @@ function App() {
         <About />
         <Projects />
         <Contact />
+        {/* <Routers>
+          <Route />
+        </Routers> */}
       </div>
     </>
   );
